c3c8d43a307e598a01c0c89b93d136b9ba948a99
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2024-02-15 03:46:43 +0600

    Select Books By Category Automation

--- automation_basic/src/test/java/com/automation/SelectBooksByCategory.java
+++ automation_basic/src/test/java/com/automation/SelectBooksByCategory.java
@@ -14,18 +14,18 @@ public class SelectBooksByCategory extends BaseDriver {
         driver.manage().window().maximize();
 		Thread.sleep(5000); // 1000 = 1sec
 		
-		driver.findElement(By.xpath("//a[@class='dropdown-toggle'][contains(text(),'লেখক')]")).click();
-		driver.findElement(By.xpath("//span[contains(text(),'কাজী নজরুল ইসলাম')]")).click();
+		driver.findElement(By.xpath("//a[@class='dropdown-toggle'][contains(text(),'বিষয়')]")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'উপন্যাস সমগ্র')]")).click();
 		Thread.sleep(2000); // 1000 = 1sec
 		
 		//Verify Page Title
 		String title=driver.getTitle();
-		String expected="কাজী নজরুল ইসলাম এর বই সমূহ অনলাইনে কিনুন | বইবাজার.কম";
+		String expected="উপন্যাস সমগ্র বিষয়ক বই সমূহ অনলাইনে কিনুন | বইবাজার.কম";
         Assert.assertEquals(title, expected, "Text does not match the expected value.");
         
 		//Verify Page Url        
 		String pageUrl=driver.getCurrentUrl();
-		String expectedUrl="https://www.boibazar.com/author-books/Kazi-Nazrul-Islam";
+		String expectedUrl="https://www.boibazar.com/category-books/novel-compilation-001";
         Assert.assertEquals(pageUrl, expectedUrl, "Text does not match the expected value.");
 		
 		}
